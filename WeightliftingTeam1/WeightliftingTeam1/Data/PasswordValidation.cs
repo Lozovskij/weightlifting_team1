df5df67c97558d2de5007f3cbe46f1e3249af639
Ivan Lozovskij <ivan.lozovskij1@gmai.com>
2020-10-31 18:07:35 +0300

    add class for password validation

---
+++ WeightliftingTeam1/WeightliftingTeam1/Data/PasswordValidation.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WeightliftingTeam1.Data
+{
+    public static class PasswordValidation
+    {
+        //SHA512
+        private const string 
+            hash = "b109f3bbbc244eb82441917ed06d618b9008dd09b3befd1b5e07394c706a8bb980b1d7785e5976ec049b46df5f1326af5a2ea6d103fd07c95385ffab0cacbc86";
+
+        private static string GetHash(string source)
+        {
+            using (SHA512 sha512Hash = SHA512.Create())
+            {
+                //From String to byte array
+                byte[] sourceBytes = Encoding.UTF8.GetBytes(source);
+                byte[] hashBytes = sha512Hash.ComputeHash(sourceBytes);
+                string hash = BitConverter.ToString(hashBytes).Replace("-", String.Empty);
+            }
+            return hash;
+        }
+
+        public static bool IsValid(string password)
+        {
+            string hashToCompare = GetHash(password);
+            return hashToCompare == hash;
+        }
+
+    }
+}
