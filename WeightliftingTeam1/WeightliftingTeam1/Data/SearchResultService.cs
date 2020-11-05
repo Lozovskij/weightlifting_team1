975b278e5fe0b93fc647cb3d229e9560d6eef8fa
Ivan Lozovskij <ivan.lozovskij1@gmai.com>
2020-11-06 00:07:25 +0300

    bug fixing and names changing

--- WeightliftingTeam1/WeightliftingTeam1/Data/SearchResultService.cs
+++ WeightliftingTeam1/WeightliftingTeam1/Data/SearchResultService.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using WeightliftingTeam1.ModelsForOutput;
 
 namespace WeightliftingTeam1.Data
 {
--- WeightliftingTeam1/WeightliftingTeam1/ModelsForOutput/AthleteResult.cs
+++ WeightliftingTeam1/WeightliftingTeam1/ModelsForOutput/AthleteResult.cs
@@ -4,7 +4,7 @@ using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace WeightliftingTeam1.Data
+namespace WeightliftingTeam1.ModelsForOutput
 {
     public class AthleteResult
     {
