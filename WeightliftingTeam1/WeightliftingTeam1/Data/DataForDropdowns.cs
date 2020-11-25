8fbfa89427131e809221301d275987907bb97846
Ivan Lozovskij <ivan.lozovskij1@gmai.com>
2020-11-25 16:08:35 +0300

    add AthletePanel

--- WeightliftingTeam1/WeightliftingTeam1/Data/DataForDropdowns.cs
+++ WeightliftingTeam1/WeightliftingTeam1/Data/DataForDropdowns.cs
@@ -7,12 +7,15 @@ namespace WeightliftingTeam1.Data
 {
     public struct DataForDropdowns
     {
-        public DataForDropdowns(IEnumerable<string> competitions, IEnumerable<string> athleteNames)
+        public DataForDropdowns(IEnumerable<string> competitions, IEnumerable<string> athleteNames, 
+            IEnumerable<string> countries)
         {
             Competitions = competitions;
+            Countries = countries;
             AthleteNames = athleteNames;
         }
         public IEnumerable<string> Competitions { get; private set; }
         public IEnumerable<string> AthleteNames { get; private set; }
+        public IEnumerable<string> Countries { get; private set; }
     }
 }
