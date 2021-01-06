c3fb717fe7c6ff639b9136823843043dfb5fffe7
Dmitry Astapchik <dmtr.astapchik@gmail.com>
2021-01-06 23:46:17 +0300

    Fix CrudService

--- WeightliftingTeam1/WeightliftingTeam1/Data/CrudService.cs
+++ WeightliftingTeam1/WeightliftingTeam1/Data/CrudService.cs
@@ -10,43 +10,40 @@ namespace WeightliftingTeam1.Data
     public class CrudService
     {
         private readonly IDbContextFactory<WeightliftingContext> _contextFactory;
-        private readonly WeightliftingContext _context;
         public CrudService(IDbContextFactory<WeightliftingContext> contextFactory)
         {
             _contextFactory = contextFactory;
-            _context = _contextFactory.CreateDbContext();
         }
 
         public void Create<TEntity>(TEntity entity)
         {
             var propertyInfo = FindProperty(typeof(TEntity));
-            propertyInfo.PropertyType.GetMethod("Add").Invoke(propertyInfo.GetValue(_context), new object[] { entity });
-            _context.SaveChanges();
+            using var context = _contextFactory.CreateDbContext();
+            propertyInfo.PropertyType.GetMethod("Add").Invoke(propertyInfo.GetValue(context), new object[] { entity });
+            context.SaveChanges();
         }
 
         public TEntity Read<TEntity>(int id)
         {
             var propertyInfo = FindProperty(typeof(TEntity));
-            return (TEntity)propertyInfo.PropertyType.GetMethod("Find").Invoke(propertyInfo.GetValue(_context),  new object[] { new object[] { id } } );
+            using var context = _contextFactory.CreateDbContext();
+            return (TEntity)propertyInfo.PropertyType.GetMethod("Find").Invoke(propertyInfo.GetValue(context),  new object[] { new object[] { id } } );
         }
 
         public void Update<TEntity>(TEntity entity)
         {
             var propertyInfo = FindProperty(typeof(TEntity));
-            var entityFound = (TEntity)propertyInfo.PropertyType.GetMethod("Find").Invoke(propertyInfo.GetValue(_context), new object[] { new object[] { typeof(TEntity).GetProperty("Id").GetValue(entity) } });
-            if (entityFound == null)
-            {
-                throw new Exception("An attempt to update an entity that is not found in the database.");
-            }
-            propertyInfo.PropertyType.GetMethod("Update").Invoke(propertyInfo.GetValue(_context), new object[] { entity });
-            _context.SaveChanges();
+            using var context = _contextFactory.CreateDbContext();
+            propertyInfo.PropertyType.GetMethod("Update").Invoke(propertyInfo.GetValue(context), new object[] { entity });
+            context.SaveChanges();
         }
 
         public void Delete<TEntity>(TEntity entity)
         {
             var propertyInfo = FindProperty(typeof(TEntity));
-            propertyInfo.PropertyType.GetMethod("Remove").Invoke(propertyInfo.GetValue(_context), new object[] { entity });
-            _context.SaveChanges();
+            using var context = _contextFactory.CreateDbContext();
+            propertyInfo.PropertyType.GetMethod("Remove").Invoke(propertyInfo.GetValue(context), new object[] { entity });
+            context.SaveChanges();
         }
 
         private System.Reflection.PropertyInfo FindProperty(Type genericArgumentType)
